8425cd4832a9956cdee69466adfde1cec8452d59
cherry <rlathrkd2766@gmail.com>
2026-03-11 15:01:43 +0900

    PGS: 문자열 돌리기

---
+++ Programmers/Lv0/PGS_181945/Solution.java
@@ -0,0 +1,14 @@
+package Lv0.PGS_181945;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i=0; i< str.length();i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}

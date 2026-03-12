109dde3bb3895b10081f216376ea6411a05ebee5
cherry <rlathrkd2766@gmail.com>
2026-03-13 01:02:25 +0900

    PGS: 더 크게 합치기

---
+++ Programmers/Lv0/PGS_181939/Solution.java
@@ -0,0 +1,11 @@
+package Lv0.PGS_181939;
+
+class Solution {
+    public int solution(int a, int b) {
+
+        int ab = Integer.parseInt(a+""+b);
+        int ba = Integer.parseInt(b+""+a);
+
+        return Math.max(ab, ba);
+    }
+}

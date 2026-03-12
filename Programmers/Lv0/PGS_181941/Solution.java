e8687c3b55e68c51ab40209dc11158404ceb2def
cherry <rlathrkd2766@gmail.com>
2026-03-13 00:39:58 +0900

    PGS: 문자 리스트를 문자열로 변환하기

---
+++ Programmers/Lv0/PGS_181941/Solution.java
@@ -0,0 +1,12 @@
+package Lv0.PGS_181941;
+
+class Solution {
+    public String solution(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}

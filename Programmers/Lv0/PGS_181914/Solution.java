4f1ad5ec312f9b08e5e2e273efba94f082eb4675
cherry <rlathrkd2766@gmail.com>
2026-03-18 12:57:19 +0900

    PGS: 9로 나눈 나머지

---
+++ Programmers/Lv0/PGS_181914/Solution.java
@@ -0,0 +1,13 @@
+package Lv0.PGS_181914;
+
+class Solution {
+    public int solution(String number) {
+
+        int sum = 0;
+
+        for (char c : number.toCharArray()) {
+            sum += c - '0';
+        }
+        return sum % 9;
+    }
+}

986104a0a4a6bfb685e6d2ef2eca12bc8797e6da
cherry <rlathrkd2766@gmail.com>
2026-03-19 20:22:05 +0900

    PGS: 문자열의 뒤의 n글자

---
+++ Programmers/Lv0/PGS_181910/Solution.java
@@ -0,0 +1,9 @@
+package Lv0.PGS_181910;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String str = my_string.substring(my_string.length() - n);
+
+        return str;
+    }
+}

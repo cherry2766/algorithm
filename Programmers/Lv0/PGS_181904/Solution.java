acc6e4949861079952fb88349dbdd31fbba7626e
cherry <rlathrkd2766@gmail.com>
2026-03-21 23:20:24 +0900

    PGS: 세로 읽기

---
+++ Programmers/Lv0/PGS_181904/Solution.java
@@ -0,0 +1,13 @@
+package Lv0.PGS_181904;
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i += m) {
+            sb.append(my_string.charAt(i + (c - 1)));
+        }
+
+        return sb.toString();
+    }
+}

da9d16e5be9f807b35a5c83f93fc43e705a752e9
cherry <rlathrkd2766@gmail.com>
2026-03-22 21:46:13 +0900

    PGS: 배열 만들기 1

---
+++ Programmers/Lv0/PGS_181901/Solution.java
@@ -0,0 +1,22 @@
+package Lv0.PGS_181901;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % k == 0) {
+                list.add(i);
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}

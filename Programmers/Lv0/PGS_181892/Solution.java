c4deebc6659e426096791978a34adea5f1e6ea40
cherry <rlathrkd2766@gmail.com>
2026-03-29 00:02:16 +0900

    PGS: n 번째 원소부터

---
+++ Programmers/Lv0/PGS_181892/Solution.java
@@ -0,0 +1,9 @@
+package Lv0.PGS_181892;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, n - 1, num_list.length);
+    }
+}

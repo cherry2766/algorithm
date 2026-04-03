30cb40f85bbcce9ef9d87055a4edad0f13954888
cherry <rlathrkd2766@gmail.com>
2026-04-03 16:38:11 +0900

    PGS: 배열에서 문자열 대소문자 변환하기

---
+++ Programmers/Lv0/PGS_181875/Solution.java
@@ -0,0 +1,16 @@
+package Lv0.PGS_181875;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+
+        for (int i = 0; i < strArr.length; i++) {
+            if (i % 2 == 0) {
+                strArr[i] = strArr[i].toLowerCase();
+            } else {
+                strArr[i] = strArr[i].toUpperCase();
+            }
+        }
+
+        return strArr;
+    }
+}

8cb89b5302712e3c01f6b80e6fec7d2a4b6a7d16
cherry <rlathrkd2766@gmail.com>
2026-04-07 11:04:55 +0900

    PGS: ad 제거하기

---
+++ Programmers/Lv0/PGS_181870/Solution.java
@@ -0,0 +1,16 @@
+package Lv0.PGS_181870;
+
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String s : strArr) {
+            if (!s.contains("ad")) {
+                list.add(s);
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+}

577b4a0fc1ebb2cba6079a0501aefa28c1a51c2c
cherry <rlathrkd2766@gmail.com>
2026-04-15 13:03:21 +0900

    PGS: 뒤에서 5등까지

---
+++ Programmers/Lv0/PGS_181853/Solution.java
@@ -0,0 +1,16 @@
+package Lv0.PGS_181853;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = num_list[i];
+        }
+
+        return arr;
+    }
+}
